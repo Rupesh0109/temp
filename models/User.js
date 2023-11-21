d5f1ad7d971eaae79eba9237073a9cf7ad8fb2fa
Rupesh0109 <rupesh2486123@gmail.com>
2023-11-21 09:15:21 +0530

    INITIAL

---
+++ models/User.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rollno: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    details: {
+        department: {
+            type: String,
+        },
+        section: {
+            type: String,
+        },
+        address: {
+            type: String,
+        },
+        bio: {
+            type: String,
+        },
+        birthday: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        mobile: {
+            type: String,
+        },
+        website: {
+            type: String,
+        },
+        cgpa: {
+            type: String,
+        },
+        bus: {
+            type: String,
+        }
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    qrsrc: {
+        type: String,
+        required: true
+    },
+    attendance: [
+        {
+            date: {
+                type: Number,
+            },
+            isPresent: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    ]
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", userSchema)
